1dc4efe9115b668efbe3c61f3ee397fc0c0bb747
Don-Ivan-117 <133603672+Don-Ivan-117@users.noreply.github.com>
2025-09-22 13:18:35 -0600

    Base Structure: Libraries and Frameworks

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { Chip } from "@heroui/react"
+function App() {
+
+  return (
+    <div className="flex gap-4">
+      <Chip color="warning" variant="solid">
+        Solid
+      </Chip>
+      <Chip color="warning" variant="bordered">
+        Bordered
+      </Chip>
+      <Chip color="warning" variant="light">
+        Light
+      </Chip>
+      <Chip color="warning" variant="flat">
+        Flat
+      </Chip>
+      <Chip color="warning" variant="faded">
+        Faded
+      </Chip>
+      <Chip color="warning" variant="shadow">
+        Shadow
+      </Chip>
+      <Chip color="warning" variant="dot">
+        Dot
+      </Chip>
+    </div>
+  )
+}
+
+export default App
